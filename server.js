6505995414536ed833a32eee25fc29abb81d1a23
Azaan Wadhwania <azaanwadhwania@yahoo.co.in>
2022-01-15 16:07:52 -0800

    added server stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
+const mongoose = require('mongoose');
 
 function logRequest(req, res, next) {
     console.log(`${new Date()}  ${req.ip} : ${req.method} ${req.path}`);
@@ -11,13 +12,24 @@ const host = 'localhost'
 const port = 3000;
 const clientApp = path.join(__dirname, 'client');
 
+// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
+
 // express app
 let app = express();
 
 app.use(express.json()) // to parse application/json
 app.use(express.urlencoded({
-    extended: true
-})) // to parse application/x-www-form-urlencoded
+        extended: true
+    })) // to parse application/x-www-form-urlencoded
 app.use(logRequest); // logging for debug
 
 // serve static files (client-side)
